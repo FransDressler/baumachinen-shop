1b2ae5d38d2e1a297ac2c03bc9172d4803db17ae
Frans Dressler <frans@Franss-Air.fritz.box>
2023-09-04 10:22:18 +0200

    changed logos

--- src/components/Home/LogoData.js
+++ src/components/Home/LogoData.js
@@ -6,6 +6,7 @@ import logo5 from '../../logos/heller-logo.png';
 import logo6 from '../../logos/hikoki-logo.png';
 import logo7 from '../../logos/husqvarna-logo.png';
 import logo8 from '../../logos/lissmac-logo.png';
+import logo9 from '../../logos/mueba-logo.png';
 import logo10 from '../../logos/rhodius-logo.png';
 import logo11 from '../../logos/sdmo-logo.png';
 import logo12 from '../../logos/wackerneuson-logo.png';
@@ -21,6 +22,7 @@ export const logoData = [
     {logo: logo6},
     {logo: logo7},
     {logo: logo8},
+    {logo: logo9},
     {logo: logo10},
     {logo: logo11},
     {logo: logo12},
